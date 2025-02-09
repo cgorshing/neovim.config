e25762c5ff2aa5eea1aa9bee0c20ea3bed2f61ef
Chad Gorshing <gorshing@gmail.com>
2025-02-09 10:45:49 -0600

    Merge conflict

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,6 +1,9 @@
 require("lualine").setup({
     options = {
+        -- icons_enabled = false,
         theme = 'onedark',
+        -- component_separators = '|',
+        -- section_separators = '',
     },
     tabline = {
         lualine_a = { 'buffers' },

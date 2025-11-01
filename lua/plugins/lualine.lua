f38fe44548ac78646c4ec50fdbb101ab6b64ee02
Chad Gorshing <gorshing@gmail.com>
2025-11-01 11:47:27 -0500

    lualine, trying to get simplistic

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,19 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    config = function()
+        require("lualine").setup({
+            options = {
+                -- icons_enabled = false,
+                theme = 'onedark',
+                -- component_separators = '|',
+                -- section_separators = '',
+            },
+            tabline = {
+                lualine_a = { 'buffers' },
+                lualine_z = { 'tabs' }
+            }
+        })
+    end,
 }

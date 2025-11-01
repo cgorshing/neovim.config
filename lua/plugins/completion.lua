9177b28dcd11e809a698953de38b67f8f1cddf30
Chad Gorshing <gorshing@gmail.com>
2025-11-01 18:33:15 -0500

    Simple setup? Maybe
    Not sure, but I think I've got this down to a small enough config that I
    can use this as a starting point. I don't understand some of the setup,
    but I will this is a point to learn from.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -6,14 +6,29 @@ return {
   -- UPDATE -- I do get the second one, but there is a delay upon
   -- first loading the buffer.
   {
-    'hrsh7th/cmp-nvim-lsp', -- do I need this now?
-  },
-  {
     'hrsh7th/nvim-cmp',
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-path", -- source for file system paths
+      {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+      },
+      "saadparwaiz1/cmp_luasnip", -- for autocompletion
+      "rafamadriz/friendly-snippets", -- useful snippets
+      "onsails/lspkind.nvim", -- vs-code like pictograms
+    },
+
     config = function()
-      -- Set up nvim-cmp.
-      local cmp = require 'cmp'
-      -- require('luasnip.loaders.from_vscode').lazy_load()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      local lspkind = require('lspkind')
+
+      require('luasnip.loaders.from_vscode').lazy_load()
 
       cmp.setup({
         window = {
@@ -26,7 +41,27 @@ return {
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+
+          -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          -- vim.keymap.set('n', '<C-h>', vim.lsp.buf.hover, opts)
+          -- vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
+          -- vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+          -- vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+          -- vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+          -- vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+          -- vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+          -- vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+          -- vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+          -- vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+
         }),
+
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           -- { name = 'vsnip' }, -- For vsnip users.
@@ -38,5 +73,5 @@ return {
         })
       })
     end
-  }
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,13 +3,13 @@ return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
         ensure_installed = {
-          "eslint",
-          "lua_ls",
-          "ts_ls",
-          "html",
-          "pyright",
           "elixirls",
+          "eslint",
+          "html",
+          "lua_ls",
+          "pyright",
           "tailwindcss",
+          "ts_ls",
         },
     },
     dependencies = {
@@ -33,16 +33,21 @@ return {
     enabled = true,
     lazy = false,
     dependencies = {
-      -- The following helps with "vim" being undefined in lua files
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      {
+        -- The following helps with "vim" being undefined in lua files
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
       },
+      {
+        "hrsh7th/cmp-nvim-lsp",
+      }
     },
 
 
@@ -51,39 +56,13 @@ return {
     -- in ~/Library/Application\ Support/.burrito
     -- Still won't work. Not sure if it is trying to run elixir/mix and the way
     -- I have asdf setup is not working
-    
+
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      -- require('lspconfig').pyright.setup({
-        -- capabilities = capabilities,
-        --  capabilities = capabilities,
-        --before_init = function(_, config)
-        -- local util = require("lspconfig/util")
-        -- local path = util.path
-        -- From https://www.youtube.com/watch?v=VljhZ0e9zGE
-        -- default_venv_path = path.join(vim.env.HOME, "virtualenvs", "nvim-venv", "bin", "python")
-        -- config.settings.python.pythonPath = default_venv_path
-        --end,
-      -- })
-
-      -- require('lspconfig').ts_ls.setup({
-        -- capabilities = capabilities,
-      -- })
-
-      -- require('lspconfig').html.setup({
-        -- capabilities = capabilities,
-      -- })
-
-      -- require('lspconfig').lua_ls.setup({
-        -- capabilities = capabilities,
-      -- })
-
-      -- require('lspconfig').elixirls.setup({
-        -- capabilities = capabilities,
-        -- cmd = { "C:\\Users\\cgorshing\\Downloads\\elixir-ls-v0.26.2\\language_server.bat" };
-        -- cmd = { "/home/cgorshing/tools/elixir-ls-built/language_server.sh" },
-      -- })
+      vim.lsp.config('*', {
+        capabilities = capabilities,
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       -- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
